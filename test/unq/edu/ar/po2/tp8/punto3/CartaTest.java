3a2cf83cecd88616bf9c09ffb025879ad6896811
Natalia <natalia.caporale@uala.com.ar>
2020-06-15 21:33:10 -0300

    Termina punto 3 tp8

--- test/unq/edu/ar/po2/tp8/punto3/CartaTest.java
+++ test/unq/edu/ar/po2/tp8/punto3/CartaTest.java
@@ -1,41 +1,80 @@
 package unq.edu.ar.po2.tp8.punto3;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 
 public class CartaTest {
 	
 	private Carta carta;
 	
-	@Mock
+	
 	private Carta cartaMock;
+	private Carta cartaMockDistintoPalo;
+	private Carta cartaMocKValorInferior;
 	
 	
 	@BeforeEach
 	public void setUp() {
-		carta = new Carta(Valor.A ,"D");
+		carta = new Carta(Valor.DOS ,"D");
+		cartaMock = mock(Carta.class);
+		cartaMockDistintoPalo = mock(Carta.class);
+		cartaMocKValorInferior = mock(Carta.class);
 		
-		Mockito.when(this.cartaMock.getPalo()).thenReturn("D");
-		Mockito.when(this.cartaMock.getValor()).thenReturn(Valor.DIEZ);
+		when(this.cartaMock.getPalo()).thenReturn("D");
+		when(this.cartaMock.getValor()).thenReturn(Valor.DIEZ);
+		
+		when(this.cartaMockDistintoPalo.getPalo()).thenReturn("T");
+		when(this.cartaMockDistintoPalo.getValor()).thenReturn(Valor.DOS);
+		
+		when(this.cartaMocKValorInferior.getPalo()).thenReturn("P");
+		when(this.cartaMocKValorInferior.getValor()).thenReturn(Valor.A);
 		
 		
 	}
 	
+	@Test
+	public void seCreaLaCartaAD(){
+		
+		assertEquals(Valor.DOS, carta.getValor());
+		assertEquals("D", carta.getPalo());
+	}
+	
 
 	@Test
-	public void elValorDeUnaCartaEsSuperiorAOtra() {
+	public void cuandoElValorDeUnaCartaEsSuperiorAOtra() {
+		
+		assertTrue(carta.esSuperiorA(cartaMocKValorInferior));
+	}
+	
+	@Test
+	public void cuandoElValorDeUnaCartaEsMenorAOtra() {
 		
 		assertFalse(carta.esSuperiorA(cartaMock));
+	}
+	
+	@Test
+	public void cuandoElValorDeUnaCartaEsIgualAlDeOtra() {
+		
+		assertFalse(carta.esSuperiorA(cartaMockDistintoPalo));
 	}
 	
 	@Test
 	public void unaCartaEsMismoPaloQueLaCartaQueRecibePorParametro() {
 		
-		assertFalse(carta.esSuperiorA(cartaMock));
+		assertTrue(carta.esMismoPaloQue(cartaMock));
+	}
+	
+	@Test
+	public void dosCartasSonDeDistintoPalo() {
+		
+		assertFalse(carta.esMismoPaloQue(cartaMockDistintoPalo));
+		
 	}
 
 }
